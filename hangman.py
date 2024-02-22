7cd27abaa189a0cbb0838850637ff0b07d65232d
Ana <106923485+AnaChkhikvadze@users.noreply.github.com>
2024-02-22 10:46:05 +0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,138 @@
+import random
+
+class HangmanGame:
+    def __init__(self):
+        self.word = self.get_word()
+        self.word_to_guess = "_" * len(self.word)
+        self.guessed = False
+        self.guessed_letters = []
+        self.state = 5
+
+    @staticmethod
+    def display_intro():
+        with open('hangman-intro.txt', 'r') as file:
+            intro_ascii = file.read()
+        print(intro_ascii)
+
+    @staticmethod
+    def display_end(result):
+        if result:
+            with open('hangman-win.txt', 'r') as file:
+                end_ascii = file.read()
+            print(end_ascii)
+        else:
+            with open('hangman-lose.txt', 'r') as file:
+                end_ascii = file.read()
+            print(end_ascii)
+
+    @staticmethod
+    def display_hangman(state):
+        hangman_states = [
+            """
+             ._______.
+             |/
+             |
+             |
+             |
+             |
+             |
+         ____|___      """,
+            """
+            ._______.
+             |/      |
+             |
+             |
+             |
+             |
+             |
+         ____|___
+
+            """,
+            """
+            ._______.
+             |/      |
+             |      (_)
+             |
+             |
+             |
+             |
+         ____|___
+            """,
+            """
+            ._______.
+             |/      |
+             |      (_)
+             |       |
+             |       |
+             |
+             |
+         ____|___
+
+            """,
+            """
+            ._______.
+             |/      |
+             |      (_)
+             |      \|/
+             |       |
+             |
+             |
+         ____|___
+            """,
+            """
+            ._______.
+             |/      |
+             |      (_)
+             |      \|/
+             |       |
+             |      / \         
+         ____|___                 
+            """
+        ]
+        return hangman_states[state]
+
+    @staticmethod
+    def get_word():
+        words = open('hangman-words.txt').read().split()
+        return random.choice(words)
+
+    @staticmethod
+    def valid(c):
+        return len(c) == 1 and 'a' <= c <= 'z'
+
+    def play(self):
+        print(self.display_hangman(self.state))
+        while not self.guessed and self.state > 0:
+            print("Guess the word: " + self.word_to_guess)
+            guess = input("Enter the letter:\n<")
+            if self.valid(guess):
+                if guess not in self.word:
+                    self.state -= 1
+                    self.guessed_letters.append(guess)
+                else:
+                    self.guessed_letters.append(guess)
+                    indexes = [i for i, letter in enumerate(self.word) if letter == guess]
+                    for i in indexes:
+                        word_as_list = list(self.word_to_guess)
+                        word_as_list[i] = guess
+                        self.word_to_guess = "".join(word_as_list)
+                    if "_" not in self.word_to_guess:
+                        self.guessed = True
+            print(self.display_hangman(self.state))
+        print("Hidden word was: " + self.word)
+        return self.guessed
+
+def hangman():
+    while True:
+        game = HangmanGame()
+        HangmanGame.display_intro()
+        result = game.play()
+        HangmanGame.display_end(result)
+        repeat = input("Do you want to play again? Yes/No")
+        while repeat.lower() not in ["yes", "no"]:
+            repeat = input("Please enter 'Yes' or 'No': ")
+        if repeat.lower() == "no":
+            break
+
+if __name__ == "__main__":
+    hangman()
